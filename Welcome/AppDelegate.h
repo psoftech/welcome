ff4553b09a6b4727b7e2320e90664057f7598227
Piyush Sharma <Piyush@piyushs-macbook-pro.local>
2016-09-23 03:46:53 -0400

    Initial Commit

---
+++ Welcome/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Welcome
+//
+//  Created by Piyush Sharma on 9/23/16.
+//  Copyright © 2016 Piyush Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Welcome/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Welcome
+//
+//  Created by Piyush Sharma on 9/23/16.
+//  Copyright © 2016 Piyush Sharma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
